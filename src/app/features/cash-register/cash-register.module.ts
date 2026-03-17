8955017a1687292b94238e479105e93a7338de47
dantegambarte <da.nt3@hotmail.com>
2026-03-17 15:53:45 -0300

    feat: add ticket modal component for sale detail view

--- src/app/features/cash-register/cash-register.module.ts
+++ src/app/features/cash-register/cash-register.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { CashRegisterRoutingModule } from './cash-register-routing.module';
 import { CashRegisterComponent } from './cash-register.component';
+import { TicketModalComponent } from './ticket-modal.component';
 
 @NgModule({
-  declarations: [CashRegisterComponent],
+  declarations: [CashRegisterComponent, TicketModalComponent],
   imports: [CommonModule, FormsModule, CashRegisterRoutingModule],
+  exports: [TicketModalComponent],
 })
 export class CashRegisterModule {}
