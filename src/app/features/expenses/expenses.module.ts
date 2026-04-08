ce4f421fbbccce000b93269c550701ffd3a05c50
dantegambarte <da.nt3@hotmail.com>
2026-04-08 16:29:57 -0300

    fix(expense-form): use modal-backdrop/modal-card classes

--- src/app/features/expenses/expenses.module.ts
+++ src/app/features/expenses/expenses.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from '../../shared/shared.module';
 import { ExpensesRoutingModule } from './expenses-routing.module';
 import { ExpensesListComponent } from './expenses-list/expenses-list.component';
 import { ExpenseFormComponent } from './expense-form/expense-form.component';
@@ -12,6 +13,7 @@ import { ExpenseFormComponent } from './expense-form/expense-form.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     ExpensesRoutingModule,
   ],
 })
