dbb811917e8d9dafb2c9656df6a227b469bbb15d
dantegambarte <da.nt3@hotmail.com>
2026-04-18 18:54:30 -0300

    feat(locale): update currency formatting to use 'es-AR' locale in internal consumption list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import localeEsAR from '@angular/common/locales/es-AR';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+registerLocaleData(localeEsAR);
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +22,7 @@ import { CoreModule } from './core/core.module';
     SharedModule,
     CoreModule,
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-AR' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
